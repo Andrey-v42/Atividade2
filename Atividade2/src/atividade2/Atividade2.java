5627a7a09161cd28c5e2379f0ddac13cc7c92909
Andrey <andrey.henrique28@gmail.com>
2019-06-19 10:43:50 -0300

    Atividade 2

---
+++ Atividade2/src/atividade2/Atividade2.java
@@ -0,0 +1,11 @@
+package atividade2;
+public class Atividade2 {
+    public static void main(String[] args) {
+        double x = 3.14;
+        if(x >= 1.99 && x <= 5.99){
+            System.out.println("X está entre 1,99 e 5,99");
+        } else {
+            System.out.println("X não está entre 1,99 e 5,99");  
+        }
+    }
+}
